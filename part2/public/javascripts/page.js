a9a97a498543c3ccd6fe9c71e1377fde2e0196cb
Austin-ADL <xiyuan.zhang01@student.adelaide.edu.au>
2025-06-20 11:33:19 +0000

    [automatic commit] 2025-06-20 11:33:18 UTC 61.69.132.41

--- part2/public/javascripts/page.js
+++ part2/public/javascripts/page.js
@@ -174,32 +174,28 @@ function downvote(index) {
 }
 
 
-function login(){
+function login() {
+  let user = {
+    email: document.getElementById('username').value,
+    password: document.getElementById('password').value
+  };
 
-    let user = {
-        user: document.getElementById('username').value,
-        pass: document.getElementById('password').value
-    };
+  var xmlhttp = new XMLHttpRequest();
 
-    // Create AJAX Request
-    var xmlhttp = new XMLHttpRequest();
+  xmlhttp.onreadystatechange = function () {
+    if (this.readyState == 4 && this.status == 200) {
+      const result = JSON.parse(this.responseText);
+      window.location.href = result.redirect;
+    } else if (this.readyState == 4 && this.status >= 400) {
+      alert("Login failed");
+    }
+  };
 
-    // Define function to run on response
-    xmlhttp.onreadystatechange = function() {
-        if (this.readyState == 4 && this.status == 200) {
-            alert("Welcome "+this.responseText);
-        } else if (this.readyState == 4 && this.status >= 400) {
-            alert("Login failed");
-        }
-    };
-
-    // Open connection to server & send the post data using a POST request
-    // We will cover POST requests in more detail in week 8
-    xmlhttp.open("POST", "/users/login", true);
-    xmlhttp.setRequestHeader("Content-type", "application/json");
-    xmlhttp.send(JSON.stringify(user));
-
+  xmlhttp.open("POST", "/users/login", true);
+  xmlhttp.setRequestHeader("Content-type", "application/json");
+  xmlhttp.send(JSON.stringify(user));
 }
+
 
 function logout(){
 
