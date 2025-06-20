8fb010d4d0165f8c84fbcc9fae13acc3478375d3
Austin-ADL <xiyuan.zhang01@student.adelaide.edu.au>
2025-06-20 07:24:41 +0000

    [automatic commit] 2025-06-20 07:24:41 UTC 130.220.242.119

--- part2/app.js
+++ part2/app.js
@@ -1,18 +1,11 @@
 const express = require('express');
 const path = require('path');
-const session = require('express-session');
 require('dotenv').config();
 
 const app = express();
 
 // Middleware
-app.use(session({
-  secret: 'dogsecret123',
-  resave: false,
-  saveUninitialized: false
-}));
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, '/public')));
 
 // Routes
