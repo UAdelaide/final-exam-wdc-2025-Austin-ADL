092879e70668ccb50d0f90fbadcd298d65f4ae07
Austin-ADL <xiyuan.zhang01@student.adelaide.edu.au>
2025-06-20 12:21:01 +0000

    [automatic commit] 2025-06-20 12:21:01 UTC 61.69.132.41

--- part2/models/db.js
+++ part2/models/db.js
@@ -1,9 +1,10 @@
 const mysql = require('mysql2/promise');
 
 const pool = mysql.createPool({
-  host: 'localhost',
-  user: 'root',
-  database: 'DogWalkService',
+  host: process.env.MYSQL_HOST || 'localhost',
+  user: process.env.MYSQL_USER || 'root',
+  password: process.env.MYSQL_PASSWORD || 'mypassword', // 添加密码
+  database: process.env.MYSQL_DATABASE || 'DogWalkService',
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0
