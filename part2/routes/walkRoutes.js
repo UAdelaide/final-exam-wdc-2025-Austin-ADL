7f231e8a0052e379b4bb78c6e8d1b0e3a24deefb
Austin-ADL <xiyuan.zhang01@student.adelaide.edu.au>
2025-06-21 04:06:22 +0000

    [automatic commit] 2025-06-21 04:06:21 UTC 130.220.243.176

--- part2/routes/walkRoutes.js
+++ part2/routes/walkRoutes.js
@@ -2,7 +2,6 @@ const express = require('express');
 const router = express.Router();
 const db = require('../models/db');
 
-// GET all walk requests (for walkers to view)
 router.get('/', async (req, res) => {
   try {
     const [rows] = await db.query(`
